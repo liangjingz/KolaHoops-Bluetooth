b8653d65095d933a38562f260c49c31b5988a0a5
Max <amclauss1@gmail.com>
2012-11-26 05:30:54 -0500

    small fix

--- com.kolahoops.bluetooth.BluetoothChat/src/com/kolahoops/bluetooth/BluetoothChat.java
+++ com.kolahoops.bluetooth.BluetoothChat/src/com/kolahoops/bluetooth/BluetoothChat.java
@@ -251,7 +251,7 @@ public class BluetoothChat extends Activity {
         Button00 = (Button) findViewById(R.id.Button00);
         Button00.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-            	sendMessage("B "+color1+" "+color2+" "+color3+" "+color4+" "+color5+" "+color6+" "+color7+color8);
+            	sendMessage("J "+color1+" "+color2+" "+color3+" "+color4+" "+color5+" "+color6+" "+color7+color8);
             }
         });
         Button01 = (Button) findViewById(R.id.Button01);
